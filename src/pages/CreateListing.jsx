2933c37681d65f0d3a0aaf021417664cef78d3ca
mdave94 <molnardavid2002@gmail.com>
2023-03-08 14:51:34 +0100

    uuid is added. Picture upload error needs to be fixed

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -4,7 +4,11 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import { useRef } from 'react'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {db} from '../firebase.config'
+
 
 const CreateListing = () => {
     const [loading, setLoading] = useState(false)
@@ -50,56 +54,109 @@ const CreateListing = () => {
         }
     }, [])
 
-    const onSubmit = async(e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
-        
+
         setLoading(true)
 
-        if(discountedPrice >= regularPrice){
+        if (discountedPrice >= regularPrice) {
             setLoading(false)
             toast.error('Higher discounted price')
-            return 
+            return
         }
 
-        if(images.lenght > 6){
+        if (images.lenght > 6) {
             setLoading(false)
             toast.error('Max 6 images')
             return
         }
 
         let geolocation = {}
-        let location 
-        
-        if(geolocationEnabled){
+        let location
+
+        if (geolocationEnabled) {
             //const response = await fetch('')
             console.log("geolocation is ON ")
-        }else{
+        } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
             location = address
 
-            
+
         }
+
+        /* Store images in FIREBASE */
+
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+              const storage = getStorage()
+              const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+      
+              const storageRef = ref(storage, 'images/' + fileName)
+      
+              const uploadTask = uploadBytesResumable(storageRef, image)
+      
+              uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                  console.log('Upload is ' + progress + '% done')
+                  switch (snapshot.state) {
+                    case 'paused':
+                      console.log('Upload is paused')
+                      break
+                    case 'running':
+                      console.log('Upload is running')
+                      break
+                    default:
+                      break
+                  }
+                },
+                (error) => {
+                  reject(error)
+                },
+                () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                  })
+                }
+              )
+            })
+          }
+
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+          ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+          })
+
+        console.log(imgUrls)
         setLoading(false)
-        
+
 
     }
 
     const onMutate = (e) => {
         let boolean = null
 
-        if(e.target.value === 'true'){
+        if (e.target.value === 'true') {
             boolean = true
         }
-        if(e.target.value === 'false'){
+        if (e.target.value === 'false') {
             boolean = false
         }
 
 
         /* FILES */
-        
-        if(e.target.files){
-            setFormData((prevState)=>({
+
+        if (e.target.files) {
+            setFormData((prevState) => ({
                 ...prevState,
                 images: e.target.files
             }))
@@ -107,10 +164,10 @@ const CreateListing = () => {
 
         /* TEXTS */
 
-        if (!e.target.files){
-            setFormData((prevState)=>({
+        if (!e.target.files) {
+            setFormData((prevState) => ({
                 ...prevState,
-            [e.target.id] : boolean ??  e.target.value
+                [e.target.id]: boolean ?? e.target.value
             }))
         }
 
@@ -278,7 +335,7 @@ const CreateListing = () => {
                         </button>
                     </div>
 
-                    <label  className="formLabel">Regular Price</label>
+                    <label className="formLabel">Regular Price</label>
                     <div className="formPriceDiv">
                         <input className='formInputSmall'
                             type='number'
